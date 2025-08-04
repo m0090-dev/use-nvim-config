97705faea7040911d4c33d805ab6fb0545440c90
m0090-dev <maru1015928@gmail.com>
2025-08-04 19:17:07 +0900

    Update

--- init.lua
+++ init.lua
@@ -5,16 +5,7 @@ require("share/plugins/require-plugins")
 require("share/command")
 require("setup")
 
--- --nopluginが指定されたかどうか
-local has_noplugin = false
 local has_minimum = os.getenv("NVIM_PLUGIN_MINIMUM") == "1"
--- Neovimの引数チェック
-for _, arg in pairs(vim.v.argv) do
-	if arg == "--noplugin" then
-		has_noplugin = true
-		break
-	end
-end
 
 Setup = Setup()
 local main_setup = Setup.new("MainSetup", require_plugins())
--- lua/share/keymap.lua
+++ lua/share/keymap.lua
@@ -1,34 +1,48 @@
+
 function set_key_maps()
-	-- [[Use Keymaps]]
+    -- [[Use Keymaps]]
 
-	-- Ctrl-Sで保存する(Windows)
-	vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true })
-	-- Space-S二回で保存する(Unix)
-	vim.api.nvim_set_keymap("n", "<leader>s", ":w ++ff=unix<CR>", { noremap = true })
-	-- Space-tで次のタブに移動
-	--vim.api.nvim_set_keymap("n", "<leader>t", ":tabnext<CR>", { noremap = true })
-	-- Space-Tで前ののタブに移動
-	--vim.api.nvim_set_keymap("n", "<leader>T", ":tabprev<CR>", { noremap = true })
+    -- Ctrl-Sで保存する(Windows)
+    vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true })
 
+    -- Space-sでUnix改行保存
+    vim.api.nvim_set_keymap("n", "<leader>s", ":w ++ff=unix<CR>", { noremap = true })
 
-	-- Space-t: 次のバッファに移動
-	vim.api.nvim_set_keymap("n", "<leader>t", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+    -- `--noplugin` モード判定（pluginが読み込まれていない＝'loaded_*'がfalse）
+    --local is_plugin_mode = vim.tbl_contains(vim.opt.runtimepath:get(), "plugin")
 
-	-- Space-T: 前のバッファに移動
-	vim.api.nvim_set_keymap("n", "<leader>T", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+    -- --nopluginが指定されたかどうか
+    local has_noplugin = false
+    -- Neovimの引数チェック
+    for _, arg in pairs(vim.v.argv) do
+	if arg == "--noplugin" then
+	   has_noplugin = true
+	   break
+ 	end
+    end
 
-	-- Space-qでタブを閉じる
-	vim.api.nvim_set_keymap("n", "<leader>q", ":BufferClose<CR>", { noremap = true })
-	vim.api.nvim_set_keymap("n", "<leader>x", ":bd!<CR>", { noremap = true, silent = true })
+    if not has_noplugin then
+        -- barbar.nvim用：Space-t / Space-T でバッファ移動
+        vim.api.nvim_set_keymap("n", "<leader>t", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>T", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
 
-	vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope<CR>", { noremap = true })
-	--vim.api.nvim_set_keymap('n', '<leader>c', ':call nerdcommenter#Comment("x", "toggle")<CR>', { noremap = true, silent = true })
+        -- バッファを閉じる
+        vim.api.nvim_set_keymap("n", "<leader>q", ":BufferClose<CR>", { noremap = true })
+        vim.api.nvim_set_keymap("n", "<leader>x", ":bd!<CR>", { noremap = true, silent = true })
 
+        -- Telescope起動
+        vim.api.nvim_set_keymap("n", "<leader>f", ":Telescope<CR>", { noremap = true })
 
-	vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true })
-	--Escapeでターミナルを抜ける
-	vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
-	vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
+        -- NvimTreeトグル
+        vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true })
 
-	vim.api.nvim_set_keymap("n", "<leader>c", ":call NERDComment('n', 'toggle')<CR>", { noremap = true })
+        -- コマンドライン補完（fine-cmdline）
+        vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", { noremap = true })
+
+        -- NERDCommenter
+        vim.api.nvim_set_keymap("n", "<leader>c", ":call NERDComment('n', 'toggle')<CR>", { noremap = true })
+    end
+
+    -- ターミナルから抜けるキー
+    vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
 end
